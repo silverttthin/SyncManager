57f7a2c3cb9086e18bd22b124cb86971fa11b95b
leesw <silverttthin@naver.com>
2024-07-16 14:59:24 +0900

    Spotify 지원 완료

--- spo.js
+++ spo.js
@@ -1,13 +1,12 @@
-console.log("This is YM content!")
-
 function getCurrentTrackInfo() {
-    const titleElement = document.querySelector('ytmusic-player-bar .title');
-    const artistElement = document.querySelector('ytmusic-player-bar .byline');
+    const titleElement = document.querySelector('[data-testid="context-item-info-title"] a')
+    const artistElement = document.querySelector('[data-testid="context-item-info-subtitles"] a');
 
     if (titleElement && artistElement) {
         const title = titleElement.textContent;
-        const artistFull = artistElement.textContent;
-        const artist = artistFull.split('•')[0].trim();
+        const artist = artistElement.textContent;
+
+        console.log(title, artist)
 
         chrome.runtime.sendMessage({
             type: 'TRACK_INFO',
@@ -23,7 +22,7 @@ function getCurrentTrackInfo() {
 }
 
 function getCurrentPlaybackTime() {
-    const timeElement = document.querySelector("#left-controls > span");
+    const timeElement = document.querySelector("#main > div > div.ZQftYELq0aOsg6tPbVbV > div.JG5J9NWJkaUO9fiKECMA > footer > div > div.sVv2OQORCQ4kf6iKfUTF > div > div.pn5V0OzovI9p6b8nWq8p.gglUjikTBtMzCZFgSmpS > div.encore-text.encore-text-marginal.encore-internal-color-text-subdued.IPbBrI6yF4zhaizFmrg6");
     if (timeElement) {
         const currentTime = timeElement.innerText.split(" / ")[0].trim();
         // console.log(`Current time: ${currentTime}`);
@@ -36,7 +35,6 @@ function getCurrentPlaybackTime() {
         console.log('재생 시각 요소를 찾을 수 없습니다.');
     }
 }
-
 
 setInterval(() => {
     getCurrentTrackInfo();
